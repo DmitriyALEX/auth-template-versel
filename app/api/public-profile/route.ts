598b89dc87c987d143f8f0443abe58d692cfe305
DmitriyALEX <doiroffical@gmail.com>
2024-10-17 18:25:40 +0300

    added routes

--- app/api/public-profile/route.ts
+++ app/api/public-profile/route.ts
@@ -1,36 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server'
-//import prisma from '@/app/helpers/prismadb'
-
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '@/app/helpers/prismadb'
 
 export async function POST(req: NextRequest) {
     try {
         const { username } = await req.json()
 
-        // const checkUsername = await prisma.username.findFirst({
-        //     where: { username: username },
-        // })
+        const checkUsername = await prisma.username.findFirst({
+            where: { username: username },
+        })
 
-        // if (!checkUsername) {
-        //     return NextResponse.json({ status: 'user not found' })
-        // }
+        if (!checkUsername) {
+            return NextResponse.json({ status: 'user not found' })
+        }
 
-        // if (checkUsername) {
-        //     const user = await prisma.user.findUnique({
-        //         where: { id: checkUsername.userId },
-        //     })
+        if (checkUsername) {
+            const user = await prisma.user.findUnique({
+                where: { id: checkUsername.userId },
+            })
 
-        //     //ONLY PUBLIC INFO
-        //     const userData = {
-        //         email: user?.email,
-        //         fullName: user?.fullName,
-        //         image: user?.image,
-        //     }
+            //ONLY PUBLIC INFO
+            const userData = {
+                email: user?.email,
+                fullName: user?.fullName,
+                image: user?.image,
+            }
 
-        //     return NextResponse.json({ status: 'user found', checkUser: userData })
-        // }
+            return NextResponse.json({ status: 'user found', checkUser: userData })
+        }
 
         return NextResponse.json({ status: 'userChecked' })
     } catch (e) {
--- app/api/set-username/route.ts
+++ app/api/set-username/route.ts
@@ -1,50 +1,46 @@
 import { NextRequest, NextResponse } from 'next/server'
-//import prisma from '@/app/helpers/prismadb'
-
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '@/app/helpers/prismadb'
 
 export async function POST(req: NextRequest) {
     try {
         const { userName, userEmail } = await req.json()
 
-        // const checkUserInfo = await prisma.user.findUnique({
-        //     where: { email: userEmail },
-        // })
+        const checkUserInfo = await prisma.user.findUnique({
+            where: { email: userEmail },
+        })
 
-        // if (checkUserInfo && checkUserInfo.id) {
-        //     //user data founded by req. email
-        //     const userData = {
-        //         id: checkUserInfo.id,
-        //         email: checkUserInfo.email,
-        //         fullName: checkUserInfo.fullName,
-        //         image: checkUserInfo.image,
-        //     }
+        if (checkUserInfo && checkUserInfo.id) {
+            //user data founded by req. email
+            const userData = {
+                id: checkUserInfo.id,
+                email: checkUserInfo.email,
+                fullName: checkUserInfo.fullName,
+                image: checkUserInfo.image,
+            }
 
-        //     const existingUsername = await prisma.username.findUnique({
-        //         where: { userId: checkUserInfo.id },
-        //     })
+            const existingUsername = await prisma.username.findUnique({
+                where: { userId: checkUserInfo.id },
+            })
 
-        //     if (existingUsername) {
-        //         const updatedUsername = await prisma.username.update({
-        //             where: { userId: checkUserInfo.id },
-        //             data: { username: userName },
-        //         })
-        //         return NextResponse.json({ success: true, checkedUsername: updatedUsername })
-        //     } else {
-        //         const createdUsername = await prisma.username.create({
-        //             data: {
-        //                 userId: checkUserInfo.id,
-        //                 username: userName,
-        //             },
-        //         })
+            if (existingUsername) {
+                const updatedUsername = await prisma.username.update({
+                    where: { userId: checkUserInfo.id },
+                    data: { username: userName },
+                })
+                return NextResponse.json({ success: true, checkedUsername: updatedUsername })
+            } else {
+                const createdUsername = await prisma.username.create({
+                    data: {
+                        userId: checkUserInfo.id,
+                        username: userName,
+                    },
+                })
 
-        //         return NextResponse.json({ success: true, checkUser: userData, checkedUsername: createdUsername })
-        //     }
-        // } else {
-        //     return NextResponse.json({ success: false })
-        // }
+                return NextResponse.json({ success: true, checkUser: userData, checkedUsername: createdUsername })
+            }
+        } else {
+            return NextResponse.json({ success: false })
+        }
 
         return NextResponse.json({ status: 'userChecked' })
     } catch (e) {
