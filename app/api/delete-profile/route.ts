0295da3ad2c95c7c552f5ced9acae800b61daf33
DmitriyALEX <doiroffical@gmail.com>
2024-10-17 17:41:03 +0300

    fix routes

--- app/api/delete-profile/route.ts
+++ app/api/delete-profile/route.ts
@@ -8,52 +8,53 @@ const prisma = new PrismaClient()
 
 export async function DELETE(req: NextRequest) {
     const { userId } = await req.json()
-    const authHeader = req.headers.get('authorization')
-    const token = authHeader ? authHeader.split(' ')[1] : null
+    // const authHeader = req.headers.get('authorization')
+    // const token = authHeader ? authHeader.split(' ')[1] : null
 
-    if (!token) {
-        return NextResponse.json({ success: false, error: 'warning' })
-    }
+    // if (!token) {
+    //     return NextResponse.json({ success: false, error: 'warning' })
+    // }
 
     try {
         //UID FROM HEADERS
-        const decodedToken = await admin.auth().verifyIdToken(token)
-        const uid = decodedToken.uid
+        // const decodedToken = await admin.auth().verifyIdToken(token)
+        // const uid = decodedToken.uid
 
-        //find user by ID from req:
-        const deleteUser = await prisma.user.findUnique({
-            where: { id: userId },
-        })
+        // //find user by ID from req:
+        // const deleteUser = await prisma.user.findUnique({
+        //     where: { id: userId },
+        // })
 
-        const verifiedUser = deleteUser?.uid === uid
+        // const verifiedUser = deleteUser?.uid === uid
 
-        if (!verifiedUser) {
-            return NextResponse.json({ success: false, error: 'user not verified' })
-        }
+        // if (!verifiedUser) {
+        //     return NextResponse.json({ success: false, error: 'user not verified' })
+        // }
 
-        if (!deleteUser) {
-            return NextResponse.json({ success: false, error: 'user not found' })
-        }
+        // if (!deleteUser) {
+        //     return NextResponse.json({ success: false, error: 'user not found' })
+        // }
 
-        if (verifiedUser) {
-            const deleteUserName = await prisma.username.findUnique({
-                where: { userId: userId },
-            })
+        // if (verifiedUser) {
+        //     const deleteUserName = await prisma.username.findUnique({
+        //         where: { userId: userId },
+        //     })
 
-            if (deleteUserName) {
-                await prisma.username.delete({
-                    where: { userId: userId },
-                })
+        //     if (deleteUserName) {
+        //         await prisma.username.delete({
+        //             where: { userId: userId },
+        //         })
 
-                await prisma.user.delete({
-                    where: { id: userId },
-                })
+        //         await prisma.user.delete({
+        //             where: { id: userId },
+        //         })
 
-                return NextResponse.json({ success: true })
-            } else {
-                NextResponse.json({ success: false, error: 'username not found' })
-            }
-        }
+        //         return NextResponse.json({ success: true })
+        //     } else {
+        //         NextResponse.json({ success: false, error: 'username not found' })
+        //     }
+        // }
+        return NextResponse.json({ status: 'userChecked' })
     } catch (e) {
         console.error(e)
         return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 })
--- app/api/public-profile/route.ts
+++ app/api/public-profile/route.ts
@@ -9,28 +9,30 @@ export async function POST(req: NextRequest) {
     try {
         const { username } = await req.json()
 
-        const checkUsername = await prisma.username.findFirst({
-            where: { username: username },
-        })
+        // const checkUsername = await prisma.username.findFirst({
+        //     where: { username: username },
+        // })
 
-        if (!checkUsername) {
-            return NextResponse.json({ status: 'user not found' })
-        }
+        // if (!checkUsername) {
+        //     return NextResponse.json({ status: 'user not found' })
+        // }
 
-        if (checkUsername) {
-            const user = await prisma.user.findUnique({
-                where: { id: checkUsername.userId },
-            })
+        // if (checkUsername) {
+        //     const user = await prisma.user.findUnique({
+        //         where: { id: checkUsername.userId },
+        //     })
 
-            //ONLY PUBLIC INFO
-            const userData = {
-                email: user?.email,
-                fullName: user?.fullName,
-                image: user?.image,
-            }
+        //     //ONLY PUBLIC INFO
+        //     const userData = {
+        //         email: user?.email,
+        //         fullName: user?.fullName,
+        //         image: user?.image,
+        //     }
 
-            return NextResponse.json({ status: 'user found', checkUser: userData })
-        }
+        //     return NextResponse.json({ status: 'user found', checkUser: userData })
+        // }
+
+        return NextResponse.json({ status: 'userChecked' })
     } catch (e) {
         console.error(e)
         return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 })
